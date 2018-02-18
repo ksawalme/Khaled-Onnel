c10d304aab052ef689c0834bb20da8ca6d60196b
Code Your Future <ksawalme@gmail.com>
2018-02-18 18:07:37 +0000

    my first git commit

--- Function.js
+++ Function.js
@@ -1,16 +1,20 @@
 
-//  function deed() {
-    
-//     myName = 'oneel'
-// }
-// console.log("hello" + myName);  
+// function welcomeMentors() {
+//     console.log("Hello Mozafar");
+//     console.log("Hello Rares");
+//     console.log("Hello Tim");
+//     console.log("Hello Ashleigh");
+//     console.log("Hello Gordon");
+// Exercise: write an improved version of this function
+
+function printHelloFiveTimes() {
+var hi = "Hello"
+for (var x = 0; x<5; x++)
+console.log(hi)
+
+}
+printHelloFiveTimes()
+
+////////////////////////////////////////
 
 
-function welcomeMentors() {
-    console.log("Hello Mozafar");
-    console.log("Hello Rares");
-    console.log("Hello Tim");
-    console.log("Hello Ashleigh");
-    console.log("Hello Gordon");
-  }
-  
