dc489eb297ef90f813f9dbd27e3c26be2e5cff06
Code Your Future <ksawalme@gmail.com>
2018-02-18 02:58:37 +0000

    < add file>

---
+++ Function.js
@@ -0,0 +1,16 @@
+
+//  function deed() {
+    
+//     myName = 'oneel'
+// }
+// console.log("hello" + myName);  
+
+
+function welcomeMentors() {
+    console.log("Hello Mozafar");
+    console.log("Hello Rares");
+    console.log("Hello Tim");
+    console.log("Hello Ashleigh");
+    console.log("Hello Gordon");
+  }
+  
