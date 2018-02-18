8a439641ee04bf4fe651b3577203205368320eac
Code Your Future <ksawalme@gmail.com>
2018-02-18 19:05:11 +0000

    Second Function

--- Function.js
+++ Function.js
@@ -7,14 +7,39 @@
 //     console.log("Hello Gordon");
 // Exercise: write an improved version of this function
 
-function printHelloFiveTimes() {
-var hi = "Hello"
-for (var x = 0; x<5; x++)
-console.log(hi)
+// function printHelloFiveTimes() {
+// var hi = "Hello"
+// for (var x = 0; x<5; x++)
+// console.log(hi)
 
-}
-printHelloFiveTimes()
+// }
+// printHelloFiveTimes()
 
 ////////////////////////////////////////
 
+// function welcomeMentors["lorenzo", ]  {
+//   console.log("Hello Mozafar");
+//   console.log("Hello Rares");
+//   console.log("Hello Tim");
+//   console.log("Hello Ashleigh");
+//   console.log("Hello Gordon");
+// }
 
+// 
+function welcomeMentors() {
+  var mentorNames = ['Mozafar', 'Rares', 'Tim', 'Ashleigh', 'Gordon']
+  mentorNames.forEach((mentor) =>{
+console.log("Hello " + mentor)
+})
+}
+welcomeMentors()
+
+// function welcomeMentors() {
+//   let arr = ['a', 'b', 'c', 'd']
+// arr.forEach((letter) => {
+//     console.log("helo " + letter)
+// })
+// }
+
+
+// welcomeMentors()
